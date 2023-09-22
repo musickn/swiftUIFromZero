b6910330471773005e30de69cd34a5546814694e
Panasarn Kanchanapiboon <panasarn.kn@gmail.com>
2023-09-22 23:39:02 +1000

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Panasarn Kanchanapiboon on 22/9/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
